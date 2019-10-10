463e2f3782390f5b1847048b9ebf744c93766dfa
vuchan.c.wu <givingwu@gmail.com>
2019-10-10 16:30:31 +0800

    update: 添加环境判断，在非开发环境不实例化Error对象

--- src/helpers/create-error.js
+++ src/helpers/create-error.js
@@ -1,9 +1,21 @@
 import { getRootApp, getAppName } from '../core/app/config'
 import { LOAD_ERROR } from '../constants/EVENT_TYPE'
+import { isDev } from '../utils/env'
 
 export function createError(error, message, code, prefix, args) {
-  if (!error || !(error instanceof Error)) {
-    error = new Error(`【${getAppName(prefix) || prefix}】：` + message)
+  if (!error) {
+    if (!(error instanceof Error)) {
+      error = new Error(`【${getAppName(prefix) || prefix}】：` + message)
+    }
+
+    // 如果非开发环境，将 ERROR 转换为普通对象，而非错误被抛出
+    if (!isDev) {
+      error = {
+        code: error.code || code,
+        message: error.message || message,
+        stack: error.stack
+      }
+    }
   }
 
   if (code && !error.code) {
